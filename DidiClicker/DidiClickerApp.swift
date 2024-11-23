dee84fbcefe3e08fe53e6a55a5a19d9c48a52fef
DidiDidi129 <136057713+DidiDidi129@users.noreply.github.com>
2024-11-24 09:45:12 +1030

    Initial Commit

---
+++ DidiClicker/DidiClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidiClickerApp.swift
+//  DidiClicker
+//
+//  Created by Didi on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DidiClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
